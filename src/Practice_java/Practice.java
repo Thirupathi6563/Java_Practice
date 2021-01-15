0800c1190e0eeb34b79e4672fa850a6fcfecfb1d
thirupathireddy247 <thirupathi6563@gmail.com>
2021-01-15 12:06:04 +0530

    First commit

---
+++ src/Practice_java/Practice.java
@@ -0,0 +1,24 @@
+package Practice_java;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Practice  {
+		public static void main(String args[])
+		{	
+	
+		}
+}
